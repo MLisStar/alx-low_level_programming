858f46aae53844467672806bfb555a0ba8ac8e66
Melissa <m3lissa.ihiga@gmail.com>
2022-12-09 08:18:31 -0800

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,9 @@
 #include <stdio.h>
-
 /**
- *
- *  * main - prints string
- *
- *   * Return: Always 0 (Sucess)
- *
- *    */
-
+ * main - prints string
+ * Return: Always 0 (Sucess)
+ */
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");			return (0);
 }
