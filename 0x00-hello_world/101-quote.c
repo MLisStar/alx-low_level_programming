afc11b462b227c3624c6ec993c3f88bdfe138a03
Melissa <m3lissa.ihiga@gmail.com>
2022-12-09 09:03:54 -0800

    description issues

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,16 +2,15 @@
 #include <string.h>
 #include <unistd.h>
 #define STRING "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-
 /**
  * main - entry point
  *
  * Return: Always 0 (success)
  */
-
 int main(void)
 {
 	int str = strlen(STRING);
+
 	write(1, STRING, str);
 	return (1);
 }
