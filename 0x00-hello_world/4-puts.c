5b8358f23574dd4316ceecaa3f2f1b25e4b18bab
Melissa <m3lissa.ihiga@gmail.com>
2022-12-08 19:00:37 -0800

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /* main - Entry point. Return: Always 0 (Success) */
 int main(void)
 {
-	//main function
+	/*maind function*/
 		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
 }
