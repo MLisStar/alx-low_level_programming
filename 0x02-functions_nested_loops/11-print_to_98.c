f10bdc6660aa08ab5b989dd0844ee989842bdf8e
Melissa <m3lissa.ihiga@gmail.com>
2022-12-13 13:26:10 -0800

    fixing issues

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,7 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d", i);
+			_putcharf('0' + i);
 			if (i != 98)
 			{
 				_putchar(44);
@@ -28,7 +28,7 @@ void print_to_98(int n)
 	{
 		for(i = n; i >= 98; i--)
 		{
-			printf("%d", i);
+			_putchar('0' + i);
 			if (i != 98)
 			{
 				_putchar(44);
