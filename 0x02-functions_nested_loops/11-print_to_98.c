dd0c0c2b3bc5a4a0cd888188e87cef14f5769ed8
Melissa <m3lissa.ihiga@gmail.com>
2022-12-14 09:17:35 -0800

    L

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,7 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-		for (i = 0; i <= 98; i++)
+		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
 			{
@@ -27,7 +27,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (i = 0; i >= 98; i--)
+		for (i = n; i >= 98; i--)
 		{
 			if (i != 98)
 			{
