c334eee7ce219d5185e13c57298694a4b175ff0a
Melissa <m3lissa.ihiga@gmail.com>
2022-12-15 08:46:47 -0800

    prime factors

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints largest prime number of 612852475143
+ * Return: 0
+ */
+int main(void)
+{
+	long int i, n, temp;
+
+	n = 612852475143;
+
+	for (i = 1; i <= n; i++)
+	{
+		if(n % i == 0)
+		{
+			if(n == 1)
+			{
+				printf("%ld\n", i);
+				break;
+			}
+			temp = n / i;
+			n = temp;
+			i = 1;
+		}
+	}
+	return (0);
+}
