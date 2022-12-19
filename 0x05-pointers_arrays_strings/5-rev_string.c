316be67db26c501cda4fec25d63bab655a8f07b0
Melissa <m3lissa.ihiga@gmail.com>
2022-12-19 14:23:12 -0800

    c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: string
+ * Return: nothing
+ *
+ */
+void rev_string(char *s)
+{
+	char temp;
+	int i, l, len;
+
+	l = 0;
+	len = 0;
+
+	while (s[l] != '\0')
+		l++;
+
+	len = l -1;
+
+	for (i = 0; i < l / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len];
+		s[len--] = temp;
+	}
+}
