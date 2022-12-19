169aeb9c0ca40d6e3bd054142d812d081ad6db1b
Melissa <m3lissa.ihiga@gmail.com>
2022-12-19 15:17:10 -0800

    adv

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * _atoi - converst string to int
+ * @s: string
+ * Return: n
+ *
+ */
+int _atoi(char *s)
+{
+	int i, j, k, len, f, nums;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	len = 0;
+	f = 0;
+	nums = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++j;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			nums = s[i] - '0';
+			if (d % 2)
+				nums = -nums;
+			k = k * 10 + nums;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
