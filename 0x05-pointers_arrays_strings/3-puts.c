105999d66cfbc4c0dfe09ee59b5bef19b99736cb
Melissa <m3lissa.ihiga@gmail.com>
2022-12-19 13:30:31 -0800

    string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - print string
+ * @str: the string
+ * Return: nothing
+ *
+ */
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str++)
+	}
+	_putchar('\n');
+}
