a935561124794dd0b703080beec12578869927fd
Melissa <m3lissa.ihiga@gmail.com>
2022-12-22 03:10:02 -0800

    print

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * print_number - print integers
+ * @n: number
+ * Return: void
+ *
+ */
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	int d = 0, i;
+
+	if (n < 0)
+		num = -n;
+	if (n == 0)
+		d = 1;
+
+	while (num >= 1)
+	{
+		num = num / 10;
+		d++;
+	}
+
+	for (i = 0; i < d; i++)
+	{
+		int pow = 1;
+		int j, k;
+
+		for (j = 0; j < k - i - 1; j++)
+		{
+			pow = pow * 10;
+		}
+
+		k = ((n / pow) % 10);
+		if (n < 0)
+		{
+			k = -k;
+			if (i == 0)
+				_putchar(45);
+		}
+		_putchar(48 + k);
+	}
+}
