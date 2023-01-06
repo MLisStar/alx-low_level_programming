741bfee0082787ca47308f3e5db3bfd5970e6801
Melissa <m3lissa.ihiga@gmail.com>
2023-01-06 07:37:25 -0800

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,11 +5,11 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (!*s)
 	{
-		_putchar(*s);
-		_putchar(*s + 1);
-	}
-	else
 		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion((s + 1));
 }
