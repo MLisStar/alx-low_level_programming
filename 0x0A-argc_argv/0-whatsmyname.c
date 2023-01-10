69ad3eeb627c6046d7362e478d2db55848dbc2c7
Melissa <m3lissa.ihiga@gmail.com>
2023-01-10 15:19:14 -0800

    prints all arguments received

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argv: array of pointers to the arguments
  * Return: Always 0.
  */
-int main(int __attribute__((__unused__)) argc, char argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 
