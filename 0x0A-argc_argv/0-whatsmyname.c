80f34087d74e44e1126b96fa30dd2bd56ed55c24
Melissa <m3lissa.ihiga@gmail.com>
2023-01-10 15:07:33 -0800

    get programs name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
-* main - prints program name then new line
-* @argc: the number of arguments
-* @argv: an array of pointers
-* Return: Always 0.
-*/
+ * main - print programs name
+ * @argc: number of arguments
+ * @argv: array of pointers
+ * Return: Always 0.
+ */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-	
+
 	return (0);
 }
