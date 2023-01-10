3e14c98da21bb28758fa85742d51c75036526ce3
Melissa <m3lissa.ihiga@gmail.com>
2023-01-10 12:18:36 -0800

    advanced tasks

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * argstostr - concat all arg of the program into a str
+ * @ac: number of arguments passed
+ * @av: array of pointers to the arguments
+ * Return: ac = 0 and av = Null or fail - NULL
+ *	Otherwise - pointer to new string
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int arg, byte, i, size = ac;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (byte = 0; av[arg][byte]; byte++)
+			size++;
+	}
+
+	str = mallo(sizeof(char) * size + 1);
+
+	if (str == NULL)
+		return (NULL);
+
+	i = 0;
+
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (byte = 0; av[arg][byte]; byte++)
+			str[i++] = av[arg][byte];
+
+		str[i++] = '\n';
+	}
+
+	str[size] = '\0';
+
+	return (str);
+}
