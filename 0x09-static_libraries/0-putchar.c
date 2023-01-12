f6bcb9a2be0035e331518ff2d73a3b32784ee148
Melissa <m3lissa.ihiga@gmail.com>
2023-01-12 08:03:58 -0800

    redo

--- 0x09-static_libraries/0-putchar.c
+++ 0x09-static_libraries/0-putchar.c
@@ -1,8 +1,8 @@
 #include <unistd.h>
 /**
  * _putchar - write the character c to stdout
- * @c: The character to print
- * Return: Always 0 (Success)
+ * @c: the character
+ * Return: Always 0
  *
  */
 int _putchar(char c)
