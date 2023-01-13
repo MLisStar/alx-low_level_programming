8ca7f971ce9f78f82c8f99a5ac22f72db52688fa
Melissa <m3lissa.ihiga@gmail.com>
2023-01-13 13:12:27 -0800

    still fixing

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,11 +1,11 @@
-#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include "main.h"
 #define ERR_MSG "Error"
 /**
- * is_digit - checks for digit
+ * is_digit - checks if string contains a non digit char
  * @s: string to be evaluated
- * Return: 0 if a non-digit if found, 1 otherwise
+ * Return: 0 if not digit is found, 1 otherwise
  *
  */
 int is_digit(char *s)
@@ -23,8 +23,8 @@ int is_digit(char *s)
 }
 /**
  * _strlen - returns the length of a string
- * @s: string to evaluate
- * Return: the length
+ * @s: string
+ * Return: the length of the string
  */
 int _strlen(char *s)
 {
@@ -32,40 +32,36 @@ int _strlen(char *s)
 
 	while (s[i] != '\0')
 	{
-		i++;
+		i++
 	}
 	return (i);
 }
 /**
- * errors - handles errors
+ * errors - handles errors for main
  */
 void errors(void)
 {
-	printf("error\n");
+	printf("Error\n");
 	exit(98);
 }
 /**
- * main - multiplies two positive numbers
+ * main - mulitiplies two positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: always 0 (success)
- *
+ * Return: 0 (success)
  */
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+	int len, len1, len2, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
-
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
-
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
 	result = malloc(sizeof(int) * len);
-
 	if (!result)
 		return (1);
 	for (i = 0; i <= len1 + len2; i++)
