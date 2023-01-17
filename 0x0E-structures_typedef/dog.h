fe6aaab4196202215365d2fb72f2ab4d55bca601
Melissa <m3lissa.ihiga@gmail.com>
2023-01-17 07:02:25 -0800

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define  DOG_H
-
+/**
+ * struct dog - a new type to describe a dog
+ * @name: name of dog
+ * @age: its age
+ * @owner: the owner
+ *
+ */
 struct dog
 {
 	char *name;
