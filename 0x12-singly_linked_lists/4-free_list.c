bccd97bd412d44b3b473c4546893d97a756031f3
MLisStar <90730254+MLisStar@users.noreply.github.com>
2023-01-30 21:43:28 +0300

    fixing free lists

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- * free_lists - frees a list_t list.
+ * free_list - frees a list_t list.
  * @head: A pointer to the list_t list.
  */
 void free_list(list_t *head)
