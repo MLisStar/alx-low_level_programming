018b92343614d31e1810044806872102eb7c7b5e
MLisStar <90730254+MLisStar@users.noreply.github.com>
2023-02-01 20:34:59 +0300

    more linked lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - print all element in a list
+ * @h: pointer in a list
+ * Return:size of the list
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (nodes);
+}
