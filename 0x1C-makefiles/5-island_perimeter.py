d64a1e156deaa019e4ee0a6a393a5cea29fb1227
MLisStar <m3lissa.ihiga@gmail.com>
2023-05-24 16:36:37 +0300

    Feature function that returns the peri of island

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module containing the island_perimeter function"""
+
+def island_perimeter(grid):
+    """returns the peri of island"""
+    peri = 0
+
+    rowNum = len(grid)
+
+    if grid != []:
+        colNum = len(grid)[0])
+
+    for a in range(rowNum):
+        for b in range(colNum):
+            if grid[a][b] == 1:
+                if (a - 1) == -1 or grid[a - 1][b] == 0:
+                    peri += 1
+                if(a + 1) == rowNum or grid[a + 1][b] == 0:
+                    peri += 1
+                if (b - 1) == -1 or grid[a][b - 1] == 0:
+                    peri += 1
+                if(b + 1) == colNum or grid[a][b + 1] == 0:
+                    peri += 1
+
+    return peri
